19250c82c231880fab46698e38343de6d2cb328a
Prasanna Ballal <prasannaballal@Prasannas-MacBook-Pro.local>
2022-03-02 14:28:23 +0100

    Update version

--- src/main/java/com/vum/PackageDemo.java
+++ src/main/java/com/vum/PackageDemo.java
@@ -4,4 +4,8 @@ public class PackageDemo {
     public static void display(String message){
         System.out.println("Hello "+message);
     }
+
+    public static void display1(String message){
+        System.out.println("Hello "+message);
+    }
 }
